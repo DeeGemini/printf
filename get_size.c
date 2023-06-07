ef2ddedae20bc26763d69fdf54dd376c214f9e61
DeeGemini <zodiactheegemini@gmail.com>
2023-06-07 07:47:34 -0700

    get_size updated

--- get_size.c
+++ get_size.c
@@ -3,24 +3,29 @@
 /**
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed
+ * @i: List of arguments to be printed.
  *
- * Return: Precision
+ * Return: Precision.
  */
+
 int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
 	int size = 0;
 
 	if (format[curr_i] == 'l')
+	{
 		size = S_LONG;
-	else if (format[curr_i] == 'o')
-		size = S_SHORT;
+		curr_i++;
+	}
 
-	if (size == 0)
-		*i = curr_i - 1;
-	else
-		*i = curr_i;
+	else if (format[curr_i] == 'h')
+	{
+		size = S_SHORT;
+		curr_i++;
+	}
+
+	*i = curr_i - 1;
 
 	return (size);
 }
